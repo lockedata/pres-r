67c1d47737efffdd50406deefdcf6e2903c9e753
Steph <Steph@itsalocke.com>
2017-09-05 23:25:52 +0100

    1st commit

--- buildpres.R
+++ buildpres.R
@@ -1,4 +1,5 @@
 library(rmarkdown)
-slides=list.files("pres","*.Rmd",full.names=TRUE)
-
+slides=sort(list.files("pres","*.Rmd",full.names=TRUE),
+            decreasing=TRUE)
+system.file("reveal.js-3.3.0.1", package = "revealjs")
 for (f in slides) render(f,output_dir = "docs")
